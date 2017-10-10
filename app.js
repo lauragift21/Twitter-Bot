15d50409915cc48a7292a1e7d676bbdeff68895a
egwuenu gift <laurandidi21@gmail.com>
2017-10-10 13:32:18 +0100

    bug fixes :sparkles:

--- app.js
+++ app.js
@@ -1,49 +1,48 @@
-
-// Dependencies =========================
-var
-twit = require('twit'),
+var twit = require('twit'),
 config = require('./config');
 
 var Twitter = new twit(config);
 
-// FAVORITE BOT====================
+// Twitter likes BOT====================
 
-// find a random tweet and 'favorite' it
-var favoriteTweet = function(){
-var params = {
-  q: '#100DaysofCode',  // REQUIRED
-  result_type: 'recent',
-  lang: 'en'
-}
-// find the tweet
-Twitter.get('search/tweets', params, function(err,data){
-
-// find tweets
-var tweet = data.statuses;
-var randomTweet = ranDom(tweet);   // pick a random tweet
-
-// if random tweet exists
-if(typeof randomTweet != 'undefined'){
-  // Tell TWITTER to 'favorite'
-  Twitter.post('favorites/create', {id: randomTweet.id_str}, function(err, response){
-    // if there was an error while 'favorite'
-    if(err){
-      console.log('CANNOT BE FAVORITE... Error');
+// find a random tweet and 'like' it
+var likeTweet = function(){
+    var params = {
+        q: '#100DaysofCode',  // REQUIRED
+        result_type: 'recent',
+        lang: 'en'
     }
-    else{
-      console.log('FAVORITED... Success!!!');
-    }
-  });
-}
-});
-}
-// grab & 'favorite' as soon as program is running...
-favoriteTweet();
-// 'favorite' a tweet in every 60 minutes
-setInterval(favoriteTweet, 3600000);
-
-// function to generate a random tweet tweet
-function ranDom (arr) {
-var index = Math.floor(Math.random()*arr.length);
-return arr[index];
-};
+    // for more parametes, see: https://dev.twitter.com/rest/reference
+  
+    // find the tweet
+    Twitter.get('search/tweets', params, function(err,data){
+  
+      // find tweets
+      var tweet = data.statuses;
+      var randomTweet = ranDom(tweet);   // pick a random tweet
+  
+      // if random tweet exists
+      if(typeof randomTweet != 'undefined'){
+        // Tell TWITTER to 'like'
+        Twitter.post('favorites/create', {id: randomTweet.id_str}, function(err, response){
+          // if there was an error while 'like'
+          if(err){
+            console.log('CANNOT BE like... Error');
+          }
+          else{
+            console.log('Great liked!!... Success!!!');
+          }
+        });
+      }
+    });
+  }
+  // grab & 'like' as soon as program is running...
+  likeTweet();
+  // 'like' a tweet in every 60 minutes
+  setInterval(likeTweet, 3600000);
+  
+  // function to generate a random tweet tweet
+  function ranDom (arr) {
+    var index = Math.floor(Math.random()*arr.length);
+    return arr[index];
+  };
