de539583a54f2dc92b85cded8e2e8e73b52f0303
egwuenu gift <laurandidi21@gmail.com>
2017-10-10 10:47:57 +0100

    added logic for liking tweets  :sparkles:

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var Twitter = new Twitter(config);
 
-//config.js
+//config.js for twitter API
 /** TWITTER APP CONFIGURATION
  * consumer_key
  * consumer_secret
@@ -8,7 +8,7 @@ var Twitter = new Twitter(config);
  * access_token_secret
  */
 module.exports = {
-    consumer_key: 'tl2cD3v13AHm7SyexuUR9d0kw',  
+    consumer_key: 'tl2cD3v13AHm7SyexuUR9d0kw',
     consumer_secret: 'oJFSD9GtOTyzTG3RLOj2c6M5sj2B3tUAfoIug4RLj54o0cJEHI',
     access_token: '3424878503-Cj20Q8nnLr4V2OvRG8yn77VDsPahS2ZX9hUuw25',  
     access_token_secret: 'k3vTEiOjGIinkBCjEHbeLWz0nKXTEUu6xDOqonAaPYtDl'
