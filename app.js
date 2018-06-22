ad8aa6215617e71879dd0bb9dbaa5f3fbf01b537
lauragift21 <laurandidi21@gmail.com>
2018-06-22 13:31:20 +0100

    Fix vulnerability issue

--- app.js
+++ app.js
@@ -1,39 +1,38 @@
 require('dotenv').config()
 
-var twit = require('twit');
-var config = require('./config');
+const twit = require('twit');
+const config = require('./config');
 
 
-var Twitter = new twit(config.twitter);
+const Twitter = new twit(config.twitter);
 // Likes Bot for Twitter ===============
 // By @lauragift21
 
 // find a random tweet and 'like' it
-var likeTweet = function(){
-    var params = {
+const likeTweet = () => {
+    const params = {
         q: '#100DaysofCode',  // REQUIRED
         result_type: 'recent',
         lang: 'en'
     }
-    // for more parametes, see: https://dev.twitter.com/rest/reference
+    // for more parameters, see: https://dev.twitter.com/rest/reference
   
     // find the tweet
-    Twitter.get('search/tweets', params, function(err,data){
-  
+    Twitter.get('search/tweets', params, (err,data) => {
       // find tweets
-      var tweet = data.statuses;
-      var randomTweet = ranDom(tweet);   // pick a random tweet
+      const tweet = data.statuses;
+      const randomTweet = ranDom(tweet);   // pick a random tweet
   
       // if random tweet exists
       if(typeof randomTweet != 'undefined'){
         // Tell TWITTER to 'like'
-        Twitter.post('favorites/create', {id: randomTweet.id_str}, function(err, response){
+        Twitter.post('favorites/create', {id: randomTweet.id_str}, (err, response) => {
           // if there was an error while 'like'
           if(err){
-            console.log('CANNOT BE like... Error');
+            console.log('Request failed');
           }
           else{
-            console.log('Great liked!!... Success!!!');
+            console.log('Awesome tweet was liked!!.');
           }
         });
       }
@@ -43,9 +42,9 @@ var likeTweet = function(){
   likeTweet();
   // 'like' a tweet in every 60 minutes
   setInterval(likeTweet, 3600000);
-  
+
   // function to generate a random tweet tweet
   function ranDom (arr) {
-    var index = Math.floor(Math.random()*arr.length);
+    const index = Math.floor(Math.random()*arr.length);
     return arr[index];
   };
